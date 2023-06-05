d135cae721c075c61406324ac8e96860ae325e68
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-06-05 05:56:24 +0000

    [Add]ヘッダーメニュー

--- nagano_cake.app/db/seeds.rb
+++ nagano_cake.app/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: "a@a" ,## 任意のメールアドレス
+  password: "aaaaaa" ## 任意のパスワード
+)
+
+
