6a01b3b6ebbb57b7ad3273d7e10f85561d0c96d4
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-06-06 13:51:28 +0000

    [Add]会員登録機能実装完了

--- nagano_cake.app/config/routes.rb
+++ nagano_cake.app/config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
-  root to: 'public/homes#top'
-  get "public/homes/about" => "homes#about", as: "/about"
+
 # 顧客用
 # URL /customers/sign_in ...
  namespace :public do
-   resources :registrations, except: [:passwords]
-   resources :sessions, except: [:passwords]
+    devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+    }
+    root to: 'homes#top'
+    get "/homes/about" => "homes#about", as: "/about"
  end
 
 # 管理者用
 # URL /admin/sign_in ...
  namespace :admin do
-   resources :sessions, except: [:registrations, :passwords]
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
  end
 
- devise_for :public
- devise_for :admin
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
