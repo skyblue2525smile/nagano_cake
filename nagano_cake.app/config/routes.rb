c650a4c66f0e9777dc3d30e63f0a1c917928ede4
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-06-07 06:51:21 +0000

    [Add]コントローラの追加

--- nagano_cake.app/config/routes.rb
+++ nagano_cake.app/config/routes.rb
@@ -1,22 +1,47 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
 # 顧客用
 # URL /customers/sign_in ...
- namespace :public do
-    devise_for :customers,skip: [:passwords], controllers: {
+devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
     }
+ scope module: :public do
+
     root to: 'homes#top'
     get "/homes/about" => "homes#about", as: "/about"
+    resources :users
  end
 
 # 管理者用
 # URL /admin/sign_in ...
- namespace :admin do
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+ namespace :admin do
+
  end
 
 
