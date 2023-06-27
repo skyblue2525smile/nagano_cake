2f13b1fe13f71d036f492c03ab5bca8cf692f306
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-06-27 12:41:45 +0000

    [商品管理実装中]

--- nagano_cake.app/app/models/item.rb
+++ nagano_cake.app/app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  def get_profile_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/flow01.png')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+   image.variant(resize_to_limit: [width, height]).processed
+  end
 end
