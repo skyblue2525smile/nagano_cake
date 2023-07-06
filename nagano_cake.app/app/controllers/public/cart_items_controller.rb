494f6cb60bc69aa40a46b1fbc070eceac8d1b9dc
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-07-06 12:53:35 +0000

    [カート機能実装中]

--- nagano_cake.app/app/controllers/public/cart_items_controller.rb
+++ nagano_cake.app/app/controllers/public/cart_items_controller.rb
@@ -1,46 +1,45 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
-    @item = Item.find(params[:id])
+    @cart_items = current_customer.cart_items
+    @sum = 0
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
     if @cart_item.update(cart_item_params)
-      redirect_to :index
+      redirect_to cart_items_path
     else
       @item = Item.find(params[:id])
-      render admin_item_path(@item.id)
+      render item_path(@item.id)
     end
   end
 
   def destroy_all
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    redirect_to :index
+    CartItem.destroy_all
+    redirect_to cart_items_path
   end
 
   def destroy
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    redirect_to :index
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_items = current_user.cart_items.all
-    @cart_items.each do |cart_item|
-    if current_customer.cart_items.find_by(item_id: @cart_item.item_id).present?
-      @cart_item.amount = cart_item.amount += @cart_item.amount
-      cart_item.update_attribute(:amount, new_amount)
+    @amount = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+      if @amount.present?
+        amount = @amount.amount + cart_item_params[:amount].to_i
+        @amount.update_attribute(:amount, amount)
+      else
+        @cart_item = CartItem.new(cart_item_params)
+        @cart_item.save
 
-    else @cart_item.save
-      redirect_to :index
-    end
+      end
+       redirect_to cart_items_path
   end
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 end
