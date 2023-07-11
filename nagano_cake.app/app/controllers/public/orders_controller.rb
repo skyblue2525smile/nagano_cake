418bfac3d8fe6ad5a1eaaaba3a2bfc5957f1bee1
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-07-11 14:42:17 +0000

    [注文機能実装中]

--- nagano_cake.app/app/controllers/public/orders_controller.rb
+++ nagano_cake.app/app/controllers/public/orders_controller.rb
@@ -1,8 +1,10 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
@@ -11,16 +13,16 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
+    binding.pry
   end
 
   def thanks
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to
   end
 
   private
