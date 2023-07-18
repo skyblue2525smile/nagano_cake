835174ef81f71de24a3cb82b2a4a3e391a89ba36
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-07-18 05:18:11 +0000

    [配送先機能実装中]

--- nagano_cake.app/app/controllers/public/addresses_controller.rb
+++ nagano_cake.app/app/controllers/public/addresses_controller.rb
@@ -1,9 +1,17 @@
 class Public::AddressesController < ApplicationController
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to address_index_path
+    else
+      @addresses = current_customer.addresses
+      render 'index'
+    end
   end
 
   def index
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
 
   def edit
@@ -11,8 +19,24 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to address_index_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    @address = current_customer.addresses
+    redirect_to address_index_path
   end
+
+  private
+  def address_params
+    params.require(:address).permit(:address_name, :address, :postal_code)
+  end
+
 end
