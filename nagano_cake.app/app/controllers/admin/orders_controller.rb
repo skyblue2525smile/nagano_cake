e852b928458174244622c70554f75536f4ffe793
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-07-23 14:12:04 +0000

    [ステータス機能実装中]

--- nagano_cake.app/app/controllers/admin/orders_controller.rb
+++ nagano_cake.app/app/controllers/admin/orders_controller.rb
@@ -10,7 +10,7 @@ class Admin::OrdersController < ApplicationController
     @order_detail = OrderDetail.where(order_id: params[:id])
 
     if @order.update(order_params)
-      @order_details.update_all(makind_stastus: 1) if @order.status == "payment_confirmation"
+      @order_details.update_all(making_stastus: 1) if @order.status == "payment_confirmation"
       ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
       # @order.status == "入金確認"
       # @order_details.each do |order_detail|
