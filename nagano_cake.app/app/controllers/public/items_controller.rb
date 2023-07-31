9ff22fd8a2191c91f4af92e806382a65e3614d41
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-07-31 13:46:48 +0000

    [ジャンル検索機能実装]

--- nagano_cake.app/app/controllers/public/items_controller.rb
+++ nagano_cake.app/app/controllers/public/items_controller.rb
@@ -1,6 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
+    
+    if params[:genre_id] # genre_id?
+      # if finded
+      @genre = Genre.find(params[:genre_id]) # get genre record
+      @items = @genre.items.page(params[:page]) # get item records
+    end
   end
 
   def show
