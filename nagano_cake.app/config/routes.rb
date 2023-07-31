1ce3668626971d4c5652429824197f62d77ee42b
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-07-31 17:16:29 +0000

    [レイアウト修正]

--- nagano_cake.app/config/routes.rb
+++ nagano_cake.app/config/routes.rb
@@ -19,12 +19,11 @@ devise_for :customers, skip: [:passwords], controllers: {
       collection { delete 'destroy_all'}
     end
     resources :addresses, only: [:index, :create, :edit, :update, :destroy]
-    resources :customers, only: [:show, :edit, :update]
-    # get '/customers/my_page' => "customers#show"
-    get '/customers/confirm' => "customers#confirm"
-    patch '/customers/:id/withdrawal' => "customers#withdrawal", as: "withdrawal"
-    # get '/customers/edit/information' => "customers#edit"
-    # patch '/customers/information/:id' => "customers#update"
+    resources :customers, only: [:show, :edit, :update] do
+     get 'confirm' => "customers#confirm"
+     patch 'withdrawal' => "customers#withdrawal"
+    end
+
 
  end
 
