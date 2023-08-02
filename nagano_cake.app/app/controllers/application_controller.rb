d451cf85fed0eac63bcd4af5079639774cc1aff6
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-08-02 09:18:14 +0000

    [レイアウト修正中]

--- nagano_cake.app/app/controllers/application_controller.rb
+++ nagano_cake.app/app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   private
 
+  def after_sign_in_path_for(resource)
+    if resource== :admin
+      admin_root_path
+    else
+      customer_path(current_customer)
+    end
+  end
+
   def after_sign_out_path_for(resource)
     if resource== :admin
       new_admin_session_path
